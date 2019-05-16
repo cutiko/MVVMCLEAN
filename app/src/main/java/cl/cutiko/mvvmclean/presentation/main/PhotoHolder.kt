7318d7c7c8805b43396e4e31fce12915c2378719
cutiko <cutiko@gmail.com>
2019-05-16 19:32:27 -0400

    ADD: pallete library make easy to get appropriate text color

---
+++ app/src/main/java/cl/cutiko/mvvmclean/presentation/main/PhotoHolder.kt
@@ -0,0 +1,36 @@
+package cl.cutiko.mvvmclean.presentation.main
+
+import android.graphics.Color
+import android.view.View
+import android.widget.TextView
+import androidx.palette.graphics.Palette
+import androidx.recyclerview.widget.RecyclerView
+import cl.cutiko.mvvmclean.R
+import cl.cutiko.mvvmclean.data.models.Photo
+import cl.cutiko.mvvmclean.presentation.views.LazyImageView
+
+class PhotoHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    companion object {
+        private const val DEFAULT_PIXELS = 100
+    }
+
+    private val lazyIv  = itemView.findViewById<LazyImageView>(R.id.lazyIv)
+    private val textView = itemView.findViewById<TextView>(R.id.descriptionTv)
+
+    fun setView(photo: Photo, fallbackText : String) {
+        lazyIv.setImage(photo.urls?.regular)
+        textView.text = photo.safeDescription(fallbackText)
+        val color = Color.parseColor(photo.color)
+        textView.setBackgroundColor(color)
+        val swatch = Palette.Swatch(color, DEFAULT_PIXELS)
+        textView.setTextColor(swatch.titleTextColor)
+    }
+
+    private fun Photo.safeDescription(fallbackText: String) = when {
+        alt_description != null -> alt_description
+        description != null -> description
+        else -> fallbackText
+    }
+
+}
--- app/src/main/java/cl/cutiko/mvvmclean/presentation/main/PhotosAdapter.kt
+++ app/src/main/java/cl/cutiko/mvvmclean/presentation/main/PhotosAdapter.kt
@@ -1,28 +1,24 @@
 package cl.cutiko.mvvmclean.presentation.main;
 
-import android.graphics.Color
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import cl.cutiko.mvvmclean.R
 import cl.cutiko.mvvmclean.data.models.Photo
-import cl.cutiko.mvvmclean.presentation.views.LazyImageView
 
-class PhotosAdapter : RecyclerView.Adapter<PhotosAdapter.PhotoHolder>() {
+class PhotosAdapter : RecyclerView.Adapter<PhotoHolder>() {
 
     private val photos = arrayListOf<Photo>()
     private lateinit var fallbackText : String
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
+        if (!::fallbackText.isInitialized) fallbackText = parent.context.getString(R.string.no_description)
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_photo, parent, false)
         return PhotoHolder(view)
     }
 
     override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
         val photo = photos[position]
-        if (!::fallbackText.isInitialized) fallbackText = holder.itemView.context.getString(R.string.no_description)
         holder.setView(photo, fallbackText)
     }
 
@@ -34,26 +30,6 @@ class PhotosAdapter : RecyclerView.Adapter<PhotosAdapter.PhotoHolder>() {
         return@let true
     } ?: run {
         return false
-    }
-
-
-    inner class PhotoHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        private val lazyIv  = itemView.findViewById<LazyImageView>(R.id.lazyIv)
-        private val textView = itemView.findViewById<TextView>(R.id.descriptionTv)
-
-        fun setView(photo: Photo, fallbackText : String) {
-            lazyIv.setImage(photo.urls?.regular)
-            textView.text = photo.safeDescription(fallbackText)
-            textView.setBackgroundColor(Color.parseColor(photo.color))
-        }
-
-    }
-
-    fun Photo.safeDescription(fallbackText: String) = when {
-        alt_description != null -> alt_description
-        description != null -> description
-        else -> fallbackText
     }
 
 }
