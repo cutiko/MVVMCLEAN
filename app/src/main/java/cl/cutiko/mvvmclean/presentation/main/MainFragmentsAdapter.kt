afb9adc0df21c43de887dcc44a9d0b63a887ed5e
cutiko <cutiko@gmail.com>
2019-05-16 16:20:25 -0400

    REFACTOR: companion object preserve class structure

--- app/src/main/java/cl/cutiko/mvvmclean/presentation/main/MainFragmentsAdapter.kt
+++ app/src/main/java/cl/cutiko/mvvmclean/presentation/main/MainFragmentsAdapter.kt
@@ -4,9 +4,11 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
-private const val FRAGMENTS = 2
-
 class MainFragmentsAdapter(fm: FragmentManager?) : FragmentPagerAdapter(fm) {
+
+    companion object {
+        private const val FRAGMENTS = 2
+    }
 
     override fun getItem(position: Int): Fragment {
         return Fragment()
