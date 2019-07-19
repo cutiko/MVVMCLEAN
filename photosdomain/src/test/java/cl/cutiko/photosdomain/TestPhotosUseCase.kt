d94071f32dbc81133cfb85f06a43bc7adc7a614f
cutiko <cutiko@gmail.com>
2019-07-19 15:18:48 -0400

    ADD: test use case

---
+++ photosdomain/src/test/java/cl/cutiko/photosdomain/TestPhotosUseCase.kt
@@ -0,0 +1,46 @@
+package cl.cutiko.photosdomain
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import cl.cutiko.data.source.rest.PhotosRestDataSource
+import cl.cutiko.domain.usecases.LiveResult
+import cl.cutiko.domain.usecases.LiveState
+import cl.cutiko.models.Photo
+import cl.cutiko.photosdomain.usecases.GetPhotosUseCase
+import junit.framework.Assert.assertNotNull
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class TestPhotosUseCase {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(mainThreadSurrogate)
+    }
+
+    @Test
+    fun test() {
+        val restSource = PhotosRestDataSource()
+        val liveResult = LiveResult<List<Photo>?>()
+        runBlocking { GetPhotosUseCase(restSource).backgroundWork(liveResult).join() }
+        val result = liveResult.value as LiveState.OnSuccess<List<Photo>?>
+        assertNotNull(result.result)
+    }
+
+    @After
+    fun cleanUp() {
+        Dispatchers.resetMain()
+        mainThreadSurrogate.close()
+    }
+
+}
