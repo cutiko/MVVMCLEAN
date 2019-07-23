5e3595f5e5856313304d65f1fb3e22252ca9519f
cutiko <cutiko@gmail.com>
2019-07-23 17:29:58 -0400

    ADD: release version

---
+++ photoslist/src/release/java/cl/cutiko/photoslist/main/PhotosListFragment.kt
@@ -0,0 +1,10 @@
+package cl.cutiko.photoslist.main
+
+class PhotosListFragment : BasePhotosListFragment() {
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = PhotosListFragment()
+    }
+
+}
