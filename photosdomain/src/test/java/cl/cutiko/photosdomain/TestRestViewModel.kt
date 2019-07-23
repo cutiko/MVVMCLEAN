ce5b86187e7a0793b9ce9d97ced851915dd8db9f
cutiko <cutiko@gmail.com>
2019-07-23 11:50:23 -0400

    ADD: testing for every viewmodel case

--- photosdomain/src/test/java/cl/cutiko/photosdomain/TestRestViewModel.kt
+++ photosdomain/src/test/java/cl/cutiko/photosdomain/TestRestViewModel.kt
@@ -2,6 +2,7 @@ package cl.cutiko.photosdomain
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import cl.cutiko.domain.usecases.LiveState
+import cl.cutiko.models.Photo
 import cl.cutiko.photosdomain.viewmodels.PhotosRestViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.newSingleThreadContext
@@ -19,6 +20,8 @@ class TestRestViewModel {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
     private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+    private val viewModel = PhotosRestViewModel()
+    private lateinit var state: Any
 
     @Before
     fun setUp() {
@@ -27,11 +30,36 @@ class TestRestViewModel {
 
     @Test
     fun testLoading() {
-        val viewModel = PhotosRestViewModel()
-        viewModel.mutablePhotos.postValue(LiveState.Loading())
+        state = LiveState.Loading<List<Photo>?>()
+        viewModel.mutablePhotos.postValue(state as LiveState.Loading<List<Photo>?>)
+        test("loading")
+    }
+
+    @Test
+    fun testError() {
+        state = LiveState.OnError<List<Photo>?>()
+        viewModel.mutablePhotos.postValue(state as LiveState.OnError<List<Photo>?>)
+        test("error")
+    }
+
+    @Test
+    fun testSuccess() {
+        state = LiveState.OnSuccess<List<Photo>?>(emptyList())
+        viewModel.mutablePhotos.postValue(state as LiveState.OnSuccess<List<Photo>?>)
+        test("success")
+    }
+
+    private fun test(message: String) {
         val value = viewModel.livePhotos.value
-        if (value !is LiveState.Loading) {
-            fail("LiveData should be loading but it wasn't: ${value.toString()}")
+        if (value != null) {
+            val actual = value::class.java.simpleName
+            val expected = state::class.java.simpleName
+            if (actual != expected) {
+                fail("The expected live data was $message but got $expected")
+            }
+            println("Test passed value was $expected")
+        } else {
+            fail("The live data value was null")
         }
     }
 
