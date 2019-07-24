6700eefa5a002f9893f9b7c87e2690a0da3753ff
cutiko <cutiko@gmail.com>
2019-07-24 16:11:08 -0400

    ADD: sucess test for photos list

--- photoslist/src/androidTest/java/cl/cutiko/photoslist/TestPhotoFragment2.kt
+++ photoslist/src/androidTest/java/cl/cutiko/photoslist/TestPhotoFragment2.kt
@@ -1,7 +1,6 @@
 package cl.cutiko.photoslist
 
 import android.content.Intent
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
@@ -11,41 +10,28 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
 import cl.cutiko.photoslist.main.MainActivity
-import cl.cutiko.photoslist.main.PhotosListFragment
 import org.junit.After
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class TestPhotoFragment2 {
 
-    @get:Rule
-    val instantTaskExecutorRule = InstantTaskExecutorRule()
-    @get:Rule
-    val activityRule = ActivityTestRule(MainActivity::class.java)
-    //private val mainThreadSurrogate = newSingleThreadContext("UI thread")
-
-
     @Before
     fun prepare() {
-        //Dispatchers.setMain(mainThreadSurrogate)
-        IdlingRegistry.getInstance().register(PhotosListFragment.idlingResource)
+        IdlingRegistry.getInstance().register(MainActivity.idlingResource)
     }
 
     @Test
     fun test() {
-        activityRule.launchActivity(Intent())
-        //onView(withId(R.id.photosPb)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        //val fragmentManager = activityRule.activity.supportFragmentManager
+        ActivityTestRule(MainActivity::class.java).launchActivity(Intent())
         onView(withId(R.id.photosRv)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
     }
 
     @After
     fun cleanUp() {
-        IdlingRegistry.getInstance().unregister(PhotosListFragment.idlingResource)
-        //Dispatchers.resetMain()
+        IdlingRegistry.getInstance().unregister(MainActivity.idlingResource)
     }
 
 }
